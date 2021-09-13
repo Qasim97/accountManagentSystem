1ccda9cd08efbbab04177697171c6c2e3914a2ab
Qasim97 <qasimjamal97@gmail.com>
2021-09-13 18:19:12 +0500

    File handling is enabled

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.Scanner;
 import java.io.FileWriter;
@@ -13,8 +14,6 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
         Scanner input = new Scanner(System.in);
-        FileWriter writer_saving = new FileWriter("saving_accounts.txt");;
-        FileWriter write_checkings;
         System.out.print("1. Create a new account\n2. Log in\n3. Specify the interest rate for saving accounts\n");
         System.out.print("Enter your choice: ");
         int userChoice = input.nextInt();
@@ -30,11 +29,24 @@ public class App
                 String acc_num = input.next();
                 System.out.print("Enter Initial Balance: ");
                 int balance = input.nextInt();
-                System.out.print("t1. Saving\n\t2. Checking\nChoose Account Type: ");
+                System.out.print("\t1. Saving\n\t2. Checking\nChoose Account Type: ");
                 int acc_type = input.nextInt();
                 if(acc_type==1){
                     Savings acc = new Savings(acc_num, name, address, phone_num, balance);
-
+                    try {
+//                        FileWriter myWriter = new FileWriter("saving_accounts.txt");
+//                        System.out.print(acc.getAccount_num() + " " + acc.getName());
+//                        myWriter.write(acc.getAccount_num() + " " + acc.getName());
+//                        myWriter.close();
+//                        System.out.println("Successfully wrote to the file.");
+                        FileWriter fileWriter = new FileWriter("saving_accounts.txt", true); //Set true for append mode
+                        PrintWriter printWriter = new PrintWriter(fileWriter);
+                        printWriter.print(acc.getAccount_num() + " " + acc.getName() + " " + acc.getPhone_num() + " " + acc.getAddress() + " " + acc.date_created + " " + acc.getBalance() + "\n");  //New line
+                        printWriter.close();
+                    } catch (IOException e) {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
                 }
                 else{
                     Checkings acc = new Checkings();
